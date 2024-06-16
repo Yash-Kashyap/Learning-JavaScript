6053b89061ee5740fb60baf79c802b833cb92cb3
Yash-Kashyap <84985521+Yash-Kashyap@users.noreply.github.com>
2024-06-16 19:05:28 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "yash"
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+//"33"=>33
+//"33afa"=>NaN
+//true=>1, false=>0
+
+let isLoggedIn = 5
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// 1 => true, number greater than 0 is always true
+// 0 => false
+
+
