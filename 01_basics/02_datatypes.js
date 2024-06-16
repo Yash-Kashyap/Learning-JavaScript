79ecd946f91f9815c55bab74db946ebc632f0a37
Yash-Kashyap <84985521+Yash-Kashyap@users.noreply.github.com>
2024-06-16 18:50:18 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("kya kr rha hai bhai tu?") //we are using nodejs, not browser
+
+console.log(3
+    +
+    5) //this is not recommended, code readability should be high
+
+let name = "Yash"
+let age = 21
+let isLoggedIn = false
+
+/*
+number
+bigint
+string
+boolean
+null = standalone value
+undefined
+symbol
+object
+*/
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
