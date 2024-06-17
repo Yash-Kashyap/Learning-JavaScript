e21a7f8c4fba8178b7715603e7873910d5c10e44
Yash-Kashyap <84985521+Yash-Kashyap@users.noreply.github.com>
2024-06-17 03:59:10 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,8 +12,59 @@ let valueInNumber = Number(score)
 
 let isLoggedIn = 5
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 // 1 => true, number greater than 0 is always true
 // 0 => false
+
+// *******************Operations***************************
+let value = 6
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2-3);
+// console.log(2+3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello";
+let str2 = " Yash";
+let str3 = str1 + str2;
+// console.log(str3); //this was normal, we all expected that
+
+/*
+but problem can be here
+*/
+
+// console.log("1"+4);
+// console.log("5"-2);
+// console.log(6*"4");
+// console.log("7"/4);
+// console.log("1"+"4");
+// console.log("1"+4+2);
+// console.log(1+4+"2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 3 - 1  //this is not preffered, as it affects readability
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+/*
+read and understand about prefix and postfix
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+*/
+
+
+
+
 
 
