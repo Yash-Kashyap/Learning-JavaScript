e548b0786f0e626a130f4ed3e85af84f4561dec8
Yash-Kashyap <84985521+Yash-Kashyap@users.noreply.github.com>
2024-06-17 16:34:00 +0000

    Math and num

--- 01_basics/08_numbersMath.js
+++ 01_basics/08_numbersMath.js
@@ -1,1 +1,31 @@
 const score = 400
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toFixed(2));
+
+const otherNumber = 123.8964
+console.log(otherNumber.toPrecision(4));
+
+const hunders = 1000000
+console.log(hunders.toLocaleString('en-IN'));
+
+//**********************************Maths********************
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.sqrt(144));
+console.log(Math.min(4,6,2,5,8));
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
